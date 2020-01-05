79b574a46548d6db562c2041aa2a01fe53837c01
DimixSqoma <dsqoma@gmail.com>
2020-01-05 20:29:56 +0100

    Library v1.2

--- src/pl/javastart/library/app/LibraryControl.java
+++ src/pl/javastart/library/app/LibraryControl.java
@@ -16,11 +16,11 @@ public class LibraryControl {
     private Library library = new Library();
 
     public void controlLoop() {
-        int option;
+        Option option;
 
         do {
             printOptions();
-            option = dataReader.getInt();
+            option = Option.createFromInt(dataReader.getInt());
             switch (option) {
                 case ADD_BOOK:
                     addBook();
@@ -40,7 +40,7 @@ public class LibraryControl {
                 default:
                     System.out.println("Nie ma takiej opcji");
             }
-        } while (option != EXIT);
+        } while (option != Option.EXIT);
     }
 
     private void printMagazines() {
@@ -68,10 +68,9 @@ public class LibraryControl {
 
     private void printOptions() {
         System.out.println("Wybierz opcję:");
-        System.out.println(EXIT + " - wyjście z programu");
-        System.out.println(ADD_BOOK + " - dodanie nowej książki");
-        System.out.println(ADD_MAGAZINE + " - dodanie nowego magazynu");
-        System.out.println(PRINT_BOOKS + " - wyświetl dostępne książki");
-        System.out.println(PRINT_MAGAZINES + " - wyświetl dostępne magazyny");
+        Option[] options = Option.values();
+        for (Option option : options) {
+            System.out.println(option);
+        }
     }
 }
