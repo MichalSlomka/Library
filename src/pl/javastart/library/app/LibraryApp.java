3486ba466c9af2f27a986ba3899a2a7c51673e1f
MichalSlomka <m.slomka3101@gmail.com>
2020-01-12 16:07:26 +0100

    Library v1.5

--- src/pl/javastart/library/app/LibraryApp.java
+++ src/pl/javastart/library/app/LibraryApp.java
@@ -1,7 +1,7 @@
 package pl.javastart.library.app;
 
 public class LibraryApp {
-    final static String APP_NAME = "Biblioteka v1.5";
+    final static String APP_NAME = "Biblioteka v1.6";
 
     //qwerty
     //123
